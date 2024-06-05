2f0ab2c5d3c0822d7d23cb2f99e1379debe96783
AlbertShang <991401495@qq.com>
2024-06-05 19:27:21 +0800

    add url embedding

---
+++ embedding_gene/url_embedding.py
@@ -0,0 +1,406 @@
+import pandas as pd
+from tld import get_tld, is_tld
+import string
+import re
+from urllib.parse import urlparse
+import ipaddress
+import tldextract
+import hashlib
+
+
+def get_url_length(url):
+    # Remove common prefixes
+    prefixes = ['http://', 'https://']
+    for prefix in prefixes:
+        if url.startswith(prefix):
+            url = url[len(prefix):]
+
+    # Remove 'www.' if present
+    url = url.replace('www.', '')
+
+    # Return the length of the remaining URL
+    return len(url)
+
+
+def extract_pri_domain(url):
+    try:
+        res = get_tld(url, as_object=True, fail_silently=False, fix_protocol=True)
+        pri_domain = res.parsed_url.netloc
+    except:
+        pri_domain = None
+    return pri_domain
+
+
+def count_letters(url):
+    num_letters = sum(char.isalpha() for char in url)
+    return num_letters
+
+
+def count_digits(url):
+    num_digits = sum(char.isdigit() for char in url)
+    return num_digits
+
+
+def count_special_chars(url):
+    special_chars = set(string.punctuation)
+    num_special_chars = sum(char in special_chars for char in url)
+    return num_special_chars
+
+
+def has_shortening_service(url):
+    pattern = re.compile(r'https?://(?:www\.)?(?:\w+\.)*(\w+)\.\w+')
+    match = pattern.search(url)
+
+    if match:
+        domain = match.group(1)
+        common_shortening_services = ['bit', 'goo', 'tinyurl', 'ow', 't', 'is',
+                                      'cli', 'yfrog', 'migre', 'ff', 'url4', 'twit',
+                                      'su', 'snipurl', 'short', 'BudURL', 'ping',
+                                      'post', 'Just', 'bkite', 'snipr', 'fic',
+                                      'loopt', 'doiop', 'short', 'kl', 'wp',
+                                      'rubyurl', 'om', 'to', 'bit', 't', 'lnkd',
+                                      'db', 'qr', 'adf', 'goo', 'bitly', 'cur',
+                                      'tinyurl', 'ow', 'bit', 'ity', 'q', 'is',
+                                      'po', 'bc', 'twitthis', 'u', 'j', 'buzurl',
+                                      'cutt', 'u', 'yourls', 'x', 'prettylinkpro',
+                                      'scrnch', 'filoops', 'vzturl', 'qr', '1url',
+                                      'tweez', 'v', 'tr', 'link', 'zip']
+
+        if domain.lower() in common_shortening_services:
+            return 1
+    return 0
+
+
+def abnormal_url(url):
+    parsed_url = urlparse(url)
+    netloc = parsed_url.netloc
+    if netloc:
+        netloc = str(netloc)
+        match = re.search(netloc, url)
+        if match:
+            return 1
+    return 0
+
+
+def secure_http(url):
+    return int(urlparse(url).scheme == 'https')
+
+
+def have_ip_address(url):
+    try:
+        parsed_url = urlparse(url)
+        if parsed_url.hostname:
+            ip = ipaddress.ip_address(parsed_url.hostname)
+            if isinstance(ip, (ipaddress.IPv4Address, ipaddress.IPv6Address)):
+                return 1
+            else:
+                return 0
+    except ValueError:
+        pass  # Invalid hostname or IP address
+
+    return 0
+
+
+def get_url_region(primary_domain):
+    ccTLD_to_region = {
+        ".ac": "Ascension Island",
+        ".ad": "Andorra",
+        ".ae": "United Arab Emirates",
+        ".af": "Afghanistan",
+        ".ag": "Antigua and Barbuda",
+        ".ai": "Anguilla",
+        ".al": "Albania",
+        ".am": "Armenia",
+        ".an": "Netherlands Antilles",
+        ".ao": "Angola",
+        ".aq": "Antarctica",
+        ".ar": "Argentina",
+        ".as": "American Samoa",
+        ".at": "Austria",
+        ".au": "Australia",
+        ".aw": "Aruba",
+        ".ax": "Åland Islands",
+        ".az": "Azerbaijan",
+        ".ba": "Bosnia and Herzegovina",
+        ".bb": "Barbados",
+        ".bd": "Bangladesh",
+        ".be": "Belgium",
+        ".bf": "Burkina Faso",
+        ".bg": "Bulgaria",
+        ".bh": "Bahrain",
+        ".bi": "Burundi",
+        ".bj": "Benin",
+        ".bm": "Bermuda",
+        ".bn": "Brunei Darussalam",
+        ".bo": "Bolivia",
+        ".br": "Brazil",
+        ".bs": "Bahamas",
+        ".bt": "Bhutan",
+        ".bv": "Bouvet Island",
+        ".bw": "Botswana",
+        ".by": "Belarus",
+        ".bz": "Belize",
+        ".ca": "Canada",
+        ".cc": "Cocos Islands",
+        ".cd": "Democratic Republic of the Congo",
+        ".cf": "Central African Republic",
+        ".cg": "Republic of the Congo",
+        ".ch": "Switzerland",
+        ".ci": "Côte d'Ivoire",
+        ".ck": "Cook Islands",
+        ".cl": "Chile",
+        ".cm": "Cameroon",
+        ".cn": "China",
+        ".co": "Colombia",
+        ".cr": "Costa Rica",
+        ".cu": "Cuba",
+        ".cv": "Cape Verde",
+        ".cw": "Curaçao",
+        ".cx": "Christmas Island",
+        ".cy": "Cyprus",
+        ".cz": "Czech Republic",
+        ".de": "Germany",
+        ".dj": "Djibouti",
+        ".dk": "Denmark",
+        ".dm": "Dominica",
+        ".do": "Dominican Republic",
+        ".dz": "Algeria",
+        ".ec": "Ecuador",
+        ".ee": "Estonia",
+        ".eg": "Egypt",
+        ".er": "Eritrea",
+        ".es": "Spain",
+        ".et": "Ethiopia",
+        ".eu": "European Union",
+        ".fi": "Finland",
+        ".fj": "Fiji",
+        ".fk": "Falkland Islands",
+        ".fm": "Federated States of Micronesia",
+        ".fo": "Faroe Islands",
+        ".fr": "France",
+        ".ga": "Gabon",
+        ".gb": "United Kingdom",
+        ".gd": "Grenada",
+        ".ge": "Georgia",
+        ".gf": "French Guiana",
+        ".gg": "Guernsey",
+        ".gh": "Ghana",
+        ".gi": "Gibraltar",
+        ".gl": "Greenland",
+        ".gm": "Gambia",
+        ".gn": "Guinea",
+        ".gp": "Guadeloupe",
+        ".gq": "Equatorial Guinea",
+        ".gr": "Greece",
+        ".gs": "South Georgia and the South Sandwich Islands",
+        ".gt": "Guatemala",
+        ".gu": "Guam",
+        ".gw": "Guinea-Bissau",
+        ".gy": "Guyana",
+        ".hk": "Hong Kong",
+        ".hm": "Heard Island and McDonald Islands",
+        ".hn": "Honduras",
+        ".hr": "Croatia",
+        ".ht": "Haiti",
+        ".hu": "Hungary",
+        ".id": "Indonesia",
+        ".ie": "Ireland",
+        ".il": "Israel",
+        ".im": "Isle of Man",
+        ".in": "India",
+        ".io": "British Indian Ocean Territory",
+        ".iq": "Iraq",
+        ".ir": "Iran",
+        ".is": "Iceland",
+        ".it": "Italy",
+        ".je": "Jersey",
+        ".jm": "Jamaica",
+        ".jo": "Jordan",
+        ".jp": "Japan",
+        ".ke": "Kenya",
+        ".kg": "Kyrgyzstan",
+        ".kh": "Cambodia",
+        ".ki": "Kiribati",
+        ".km": "Comoros",
+        ".kn": "Saint Kitts and Nevis",
+        ".kp": "Democratic People's Republic of Korea (North Korea)",
+        ".kr": "Republic of Korea (South Korea)",
+        ".kw": "Kuwait",
+        ".ky": "Cayman Islands",
+        ".kz": "Kazakhstan",
+        ".la": "Laos",
+        ".lb": "Lebanon",
+        ".lc": "Saint Lucia",
+        ".li": "Liechtenstein",
+        ".lk": "Sri Lanka",
+        ".lr": "Liberia",
+        ".ls": "Lesotho",
+        ".lt": "Lithuania",
+        ".lu": "Luxembourg",
+        ".lv": "Latvia",
+        ".ly": "Libya",
+        ".ma": "Morocco",
+        ".mc": "Monaco",
+        ".md": "Moldova",
+        ".me": "Montenegro",
+        ".mf": "Saint Martin (French part)",
+        ".mg": "Madagascar",
+        ".mh": "Marshall Islands",
+        ".mk": "North Macedonia",
+        ".ml": "Mali",
+        ".mm": "Myanmar",
+        ".mn": "Mongolia",
+        ".mo": "Macao",
+        ".mp": "Northern Mariana Islands",
+        ".mq": "Martinique",
+        ".mr": "Mauritania",
+        ".ms": "Montserrat",
+        ".mt": "Malta",
+        ".mu": "Mauritius",
+        ".mv": "Maldives",
+        ".mw": "Malawi",
+        ".mx": "Mexico",
+        ".my": "Malaysia",
+        ".mz": "Mozambique",
+        ".na": "Namibia",
+        ".nc": "New Caledonia",
+        ".ne": "Niger",
+        ".nf": "Norfolk Island",
+        ".ng": "Nigeria",
+        ".ni": "Nicaragua",
+        ".nl": "Netherlands",
+        ".no": "Norway",
+        ".np": "Nepal",
+        ".nr": "Nauru",
+        ".nu": "Niue",
+        ".nz": "New Zealand",
+        ".om": "Oman",
+        ".pa": "Panama",
+        ".pe": "Peru",
+        ".pf": "French Polynesia",
+        ".pg": "Papua New Guinea",
+        ".ph": "Philippines",
+        ".pk": "Pakistan",
+        ".pl": "Poland",
+        ".pm": "Saint Pierre and Miquelon",
+        ".pn": "Pitcairn",
+        ".pr": "Puerto Rico",
+        ".ps": "Palestinian Territory",
+        ".pt": "Portugal",
+        ".pw": "Palau",
+        ".py": "Paraguay",
+        ".qa": "Qatar",
+        ".re": "Réunion",
+        ".ro": "Romania",
+        ".rs": "Serbia",
+        ".ru": "Russia",
+        ".rw": "Rwanda",
+        ".sa": "Saudi Arabia",
+        ".sb": "Solomon Islands",
+        ".sc": "Seychelles",
+        ".sd": "Sudan",
+        ".se": "Sweden",
+        ".sg": "Singapore",
+        ".sh": "Saint Helena",
+        ".si": "Slovenia",
+        ".sj": "Svalbard and Jan Mayen",
+        ".sk": "Slovakia",
+        ".sl": "Sierra Leone",
+        ".sm": "San Marino",
+        ".sn": "Senegal",
+        ".so": "Somalia",
+        ".sr": "Suriname",
+        ".ss": "South Sudan",
+        ".st": "São Tomé and Príncipe",
+        ".sv": "El Salvador",
+        ".sx": "Sint Maarten (Dutch part)",
+        ".sy": "Syria",
+        ".sz": "Eswatini",
+        ".tc": "Turks and Caicos Islands",
+        ".td": "Chad",
+        ".tf": "French Southern Territories",
+        ".tg": "Togo",
+        ".th": "Thailand",
+        ".tj": "Tajikistan",
+        ".tk": "Tokelau",
+        ".tl": "Timor-Leste",
+        ".tm": "Turkmenistan",
+        ".tn": "Tunisia",
+        ".to": "Tonga",
+        ".tr": "Turkey",
+        ".tt": "Trinidad and Tobago",
+        ".tv": "Tuvalu",
+        ".tw": "Taiwan",
+        ".tz": "Tanzania",
+        ".ua": "Ukraine",
+        ".ug": "Uganda",
+        ".uk": "United Kingdom",
+        ".us": "United States",
+        ".uy": "Uruguay",
+        ".uz": "Uzbekistan",
+        ".va": "Vatican City",
+        ".vc": "Saint Vincent and the Grenadines",
+        ".ve": "Venezuela",
+        ".vg": "British Virgin Islands",
+        ".vi": "U.S. Virgin Islands",
+        ".vn": "Vietnam",
+        ".vu": "Vanuatu",
+        ".wf": "Wallis and Futuna",
+        ".ws": "Samoa",
+        ".ye": "Yemen",
+        ".yt": "Mayotte",
+        ".za": "South Africa",
+        ".zm": "Zambia",
+        ".zw": "Zimbabwe"
+    }
+
+    for ccTLD in ccTLD_to_region:
+        if primary_domain.endswith(ccTLD):
+            return ccTLD_to_region[ccTLD]
+
+    return "Global"
+
+
+def extract_root_domain(url):
+    extracted = tldextract.extract(url)
+    root_domain = extracted.domain
+    return root_domain
+
+
+def hash_encode(category):
+    hash_object = hashlib.md5(category.encode())
+    return int(hash_object.hexdigest(), 16) % (10 ** 8)
+
+
+urls_data = pd.read_csv('dataset/malicious_phish.csv')
+urls_data['url'] = urls_data['url'].replace('www.', '', regex=True)
+urls_data['pri_domain'] = urls_data['url'].apply(lambda x: extract_pri_domain(x))
+urls_data["url_type"] = urls_data["type"].replace({
+    'benign': 0,
+    'defacement': 1,
+    'phishing': 1,
+    'malware': 1
+})
+
+urls_data['url_len'] = urls_data['url'].apply(lambda x: get_url_length(str(x)))
+urls_data['letters_count'] = urls_data['url'].apply(lambda x: count_letters(x))
+urls_data['digits_count'] = urls_data['url'].apply(lambda x: count_digits(x))
+urls_data['special_chars_count'] = urls_data['url'].apply(lambda x: count_special_chars(x))
+urls_data['shortened'] = urls_data['url'].apply(lambda x: has_shortening_service(x))
+urls_data['secure_http'] = urls_data['url'].apply(lambda x: secure_http(x))
+urls_data['have_ip'] = urls_data['url'].apply(lambda x: have_ip_address(x))
+urls_data['url_region'] = urls_data['pri_domain'].apply(lambda x: get_url_region(str(x)))
+urls_data['root_domain'] = urls_data['pri_domain'].apply(lambda x: extract_root_domain(str(x)))
+urls_data['abnormal_url'] = urls_data['url'].apply(lambda x: abnormal_url(x))
+urls_data.fillna(0, inplace=True)
+urls_data.drop_duplicates(inplace=True)
+data = urls_data.drop(columns=['url', 'type', 'pri_domain'])
+data = data[data['root_domain'] != '0']
+data['root_domain'] = data['root_domain'].apply(hash_encode)
+data['url_region'] = data['url_region'].apply(hash_encode)
+
+counts = urls_data["url_type"].value_counts()
+malicious_cnt = counts[1]
+benign_cnt = counts[0]
+
+data.to_csv('dataset/train.csv', index=False)
