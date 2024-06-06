468347e17202c3a1b03d25903c2139d689c171c1
cquhx <674553522@qq.com>
2024-06-06 14:13:09 +0800

    debug lgb for url

--- lgb_url_main.py
+++ lgb_url_main.py
@@ -2,46 +2,29 @@ import lightgbm as lgb
 import numpy as np
 import pandas as pd
 from bayes_opt import BayesianOptimization
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
-import lib.lgb_url
 import lib.bf_util
+import lib.lgb_url
 
-# 加载数据集
-df = pd.read_csv('dataset/train.csv')
 
-# 将's'标签（1）和'b'标签（0）分别过滤出来
-df_s = df[df['url_type'] == 1]
-df_b = df[df['url_type'] == 0]
-# 取出标签为's'的所有样本数
-s_count = len(df_s)
+df_train = pd.read_csv('dataset/url_train.csv')
+df_test = pd.read_csv('dataset/url_test.csv')
+df_query = pd.read_csv('dataset/url_query.csv')
 
-# 从标签为'b'的样本中随机抽取和's'标签相同数量的样本
-# df_b_sample = df_b.sample(n=s_count, random_state=42)
-df_b_sample = df_b.sample(frac=0.8, random_state=42)
+train_urls = df_train['url']
+test_urls = df_test['url']
+query_urls = df_query['url']
 
-# 合并得到训练集（1:1比例）
-train_df = pd.concat([df_s, df_b_sample])
+X_train = df_train.drop(columns=['url', 'url_type']).values.astype(np.float32)
+y_train = df_train['url_type'].values.astype(np.float32)
+X_test = df_test.drop(columns=['url', 'url_type']).values.astype(np.float32)
+y_test = df_test['url_type'].values.astype(np.float32)
+X_query = df_query.drop(columns=['url', 'url_type']).values.astype(np.float32)
+y_query = df_query['url_type'].values.astype(np.float32)
 
-# 剩下的标签为'b'的样本作为测试集
-validate_df = df_b.drop(df_b_sample.index)
-
-# 如果需要拆分特征和标签，可以如下进行
-X = train_df.drop('url_type', axis=1).values.astype(np.float32)
-y = train_df['url_type'].values.astype(np.float32)
-X_query = validate_df.drop('url_type', axis=1).values.astype(np.float32)
-y_query = validate_df['url_type'].values.astype(np.float32)
-
-# # 数据标准化
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-X_query = scaler.fit_transform(X_query)
-#
-# # 划分训练集和测试集
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 train_data = lgb.Dataset(X_train, label=y_train, free_raw_data=False)
 test_data = lgb.Dataset(X_test, label=y_test, free_raw_data=False)
+
 
 # 设置参数
 params = {
@@ -53,8 +36,8 @@ params = {
 }
 all_memory = 128 * 1024  # tweet模型大小：5 * 1024 * 1024
 
-n_true = s_count
-n_test = len(X_test)
+n_true = df_train[df_train['url_type'] == 1].shape[0] + df_test[df_test['url_type'] == 1].shape[0]
+n_test = len(df_test)
 
 
 def evaluate_threshold(thresh, y_pred, y_true, bf_bytes, thresh_fpr_map):
@@ -73,7 +56,7 @@ bst = None
 best_bst = None
 best_fpr = 1.0
 best_threshold = 0.5
-epoch_each = 1
+epoch_each = 2
 epoch_now = 0
 epoch_max = 20
 best_epoch = 0
@@ -118,7 +101,7 @@ print("模型在内存中所占用的大小（字节）:", model_size)
 print(f"best threshold:", best_threshold)
 print(f"best epoch:", best_epoch)
 
-data_negative = lib.lgb_url.lgb_validate(best_bst, X_train, y_train, X_test, y_test, best_threshold)
+data_negative = lib.lgb_url.lgb_validate(best_bst, X_train, y_train, train_urls, X_test, y_test, test_urls, best_threshold)
 bloom_size = all_memory - model_size
 
 bloom_filter = lib.lgb_url.create_bloom_filter(dataset=data_negative, bf_size=bloom_size)
@@ -131,4 +114,4 @@ memory_in_bytes = num_bits / 8
 print("memory of bloom filter: ", memory_in_bytes)
 print("memory of learned model: ", model_size)
 
-fpr = lib.lgb_url.lgb_query(best_bst, bloom_filter, X_query, y_query, best_threshold, False)
+fpr = lib.lgb_url.lgb_query(best_bst, bloom_filter, X_query, y_query, query_urls, best_threshold, False)
--- lib/lgb_url.py
+++ lib/lgb_url.py
@@ -14,14 +14,22 @@ def lgb_get_model_size(lgb_model):
     return model_size
 
 
-def lgb_validate(model, X_train, y_train, X_test, y_test, threshold):
+def lgb_validate(model, X_train, y_train, train_urls, X_test, y_test, test_urls, threshold):
     prediction_results = model.predict(X_train)
     binary_predictions = (prediction_results > threshold).astype(int)
     indices_train = [i for i in range(len(X_train)) if binary_predictions[i] == 0 and y_train[i] == 1]
+
+    # 对测试集进行预测
     prediction_results = model.predict(X_test)
     binary_predictions = (prediction_results > threshold).astype(int)
     indices_test = [i for i in range(len(X_test)) if binary_predictions[i] == 0 and y_test[i] == 1]
-    return np.concatenate((X_train[indices_train], X_test[indices_test]), axis=0)
+
+    # 提取对应的url
+    urls_train = train_urls.iloc[indices_train].values
+    urls_test = test_urls.iloc[indices_test].values
+
+    # 返回合并后的url数组
+    return np.concatenate((urls_train, urls_test), axis=0)
 
 
 def create_bloom_filter(dataset, bf_size):
@@ -37,7 +45,7 @@ def create_bloom_filter(dataset, bf_size):
     return bloom_filter
 
 
-def lgb_query(model, bloom_filter, X_query, y_query, threshold, draw):
+def lgb_query(model, bloom_filter, X_query, y_query, query_urls, threshold, draw):
     fn = 0
     fp = 0
     cnt_ml = 0
@@ -64,15 +72,15 @@ def lgb_query(model, bloom_filter, X_query, y_query, threshold, draw):
     print(binary_predictions)
 
     for i in range(total):
-        input_data = X_query[i]
         true_label = y_query[i]
+        url = query_urls[i]
         prediction = binary_predictions[i]
         if prediction == 1:
             if true_label == 0:
                 fp = fp + 1
                 cnt_ml = cnt_ml + 1
         else:
-            if input_data in bloom_filter:
+            if url in bloom_filter:
                 if true_label == 0:
                     fp = fp + 1
                     cnt_bf = cnt_bf + 1
